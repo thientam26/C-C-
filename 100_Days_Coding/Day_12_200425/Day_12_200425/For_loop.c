46ca88c8495f71ad8d4a7ea39cd976a36b0794e5
Nguyen Thien Tam <thientam.2695@gmail.com>
2020-04-26 00:24:50 +0700

    Day_12_200425

---
+++ 100_Days_Coding/Day_12_200425/Day_12_200425/For_loop.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<stdint.h>
+
+int  main()
+{
+	/*phan thuc thi vong lap for*/
+	int a;
+	for (a = 3; a < 16; a = a + 1)
+	{
+		printf("gia tri cua a la: %d\r\n", a);
+	}
+
+	printf("\n====================\n");
+	printf("thank you");
+	return 0;
+
+}
+
+//int main() {
+//
+//    int i;
+//
+//    /* for loop execution */
+//    for (i = 10; i < 20; i++) {
+//        printf("i: %d\n", i);
+//    }
+//
+//    return 0;
+//}
