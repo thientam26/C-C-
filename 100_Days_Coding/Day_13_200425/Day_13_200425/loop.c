a8073a57cb71a722a4105ccee718532834ea7bec
Nguyen Thien Tam <thientam.2695@gmail.com>
2020-04-26 00:26:05 +0700

    Day_13_200425

---
+++ 100_Days_Coding/Day_13_200425/Day_13_200425/loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    /* phan thuc thi vong lap for */
+    for (a = 4; a < 16; a = a + 1)
+    {
+        printf("Gia tri cua a la: %d\n", a);
+    }
+    printf("\n===========================\n");
+    printf("thank you \n");
+
+    return 0;
+}
