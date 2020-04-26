ab7d4b8bb44401ee28adbb0879ed203b34208585
Nguyen Thien Tam <thientam.2695@gmail.com>
2020-04-26 16:09:42 +0700

    Update 200426

--- TM4C123GXL/TIVA_Template/main.c
+++ TM4C123GXL/TIVA_Template/main.c
@@ -1,5 +1,5 @@
 /**
- *Chuong trinh bat tat LED
+ *Chuong trinh bat tat theo thu tu 3  LED
  */
 #include<stdint.h>
 #include<stdbool.h>
@@ -11,14 +11,26 @@
 //Chuong trinh chinh
 int main(void){
 
+    // Khai bao chuong trinh voi bien shift co nhiem vu dich bit
+
+    uint8_t shift = GPIO_PIN_1;
+    // Khoi tao clock he thong
 	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+	// Kich hoat ngoai vi
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);  //Cau hinh port
-	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_1);  // Cau hinh su dung Pin 1 cua port F, chan nay noi voi led do co san treb led
+	// Cau hinh output xuat LED
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);  // Cau hinh su dung Pin 1 cua port F, chan nay noi voi led do co san treb led
 	while(1)
 	{
-	    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1); //bat den LED
+	    // Bat LED do
+	    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, shift); //bat den LED
 	    SysCtlDelay(5000000); // Delay
-	    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1,0);//Tat den LED
-	    SysCtlDelay(5000000);
+	    // Dich bit voi y nghia bat lED xanh
+	    shift <<= 1;
+	    // Kiem tra neu LED xanh la sang thi quay tro lai LED do
+	    if (shift > GPIO_PIN_3)
+	        shift = GPIO_PIN_1;
+	    // GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1,0);//Tat den LED
+	    // SysCtlDelay(5000000);
 	}
 }
