7540b392f6d026972d1fadb0fc08acc720907345
Nguyen Thien Tam <thientam.2695@gmail.com>
2020-04-29 21:05:03 +0700

    update

--- Day_7_200429_while/Day_7_200429_while/while_do_while.c
+++ Day_7_200429_while/Day_7_200429_while/while_do_while.c
@@ -6,13 +6,18 @@ void main()
 	int i = 0;
 	while (i <= 10)
 	{
-		printf("So %d\r\n", i);
-		i++;
+		printf("So %d\r\n", i++);
+		//i++;
 	}
 	int j = 0;
 	do
 	{
-		printf("do while %d\r\n", j);
-		j++;
+		printf("do while %d\r\n", ++j); // thuc hien thao tac cong truoc khi in ra
+		
 	} while (j <= 10);
+	int k = 0;
+	for (k = 0; k <= 10; k++)
+	{
+		printf("For %d\r\n", k);
+	}
 }
