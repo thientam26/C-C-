2c3434aa208c38935047c49aaa56ec86d7c90efd
Nguyen Thien Tam <thientam.2695@gmail.com>
2021-01-17 00:03:53 +0700

    Chuong tring timer va interrupt

---
+++ TM4C123GXL/Template_3/main.c
@@ -0,0 +1,18 @@
+/**
+ * Chuong tring su dung ket hop timer voi ngat de chop tat LED BLUE voi tan so cao
+ **/
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+int main(void)
+{
+	uint32_t ui32Period ;
+	SysCtlClockSet (Sysctl)
+}
