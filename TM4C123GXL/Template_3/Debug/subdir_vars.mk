be2cd17a4043cfbd991f312099aaf6ea958e64a3
Nguyen Thien Tam <thientam.2695@gmail.com>
2021-01-19 23:01:58 +0700

    Fix error

--- TM4C123GXL/Template_3/Debug/subdir_vars.mk
+++ TM4C123GXL/Template_3/Debug/subdir_vars.mk
@@ -8,6 +8,9 @@ SHELL = cmd.exe
 CMD_SRCS += \
 ../tm4c123gh6pm.cmd 
 
+LIB_SRCS += \
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib 
+
 C_SRCS += \
 ../main.c \
 ../tm4c123gh6pm_startup_ccs.c 
--- TM4C123GXL/Template_3/main.c
+++ TM4C123GXL/Template_3/main.c
@@ -3,16 +3,18 @@
  **/
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/tm4c123gh6pm.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/gpio.h"
-#include "driverlib/timer.h"
+#include <inc/tm4c123gh6pm.h>
+#include <inc/hw_memmap.h>
+#include <inc/hw_types.h>
+#include <driverlib/sysctl.h>
+#include <driverlib/interrupt.h>
+#include <driverlib/gpio.h>
+#include <driverlib/timer.h>
 
 int main(void)
 {
-	uint32_t ui32Period ;
-	SysCtlClockSet (Sysctl)
+    uint32_t ui32Period ;
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL| SYSCTL_XTAL_16MHZ);
+
+
 }
